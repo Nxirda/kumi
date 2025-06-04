ec2368012467e5629098424d7799dc2197f3bd08
Nxirda <Nxirda@users.noreply.github.com>
2025-06-04 16:05:40 +0200

    Cartesian product
    Weird behavior on the types there are things to fix here also
    (probably missing few logical operators inside member_capture)

--- test/unit/cartesian_product.cpp
+++ test/unit/cartesian_product.cpp
@@ -29,6 +29,28 @@ TTS_CASE("Check result::cartesian_product<Tuple...> behavior")
               );
 };
 
+TTS_CASE("Check result::cartesian_product<Tuple...> behavior on named tuples")
+{
+  using namespace kumi::literals;
+  using f1 = decltype("x"_m = 1     );
+  using f2 = decltype("y"_m = 2.f   );
+  using f3 = decltype("z"_m = 3.    );
+
+  TTS_TYPE_IS ( (kumi::result::cartesian_product_t<kumi::tuple<f1,f2,f3>>)
+              , (kumi::tuple<kumi::tuple<int>,kumi::tuple<float>,kumi::tuple<double>>)
+              );
+
+  TTS_TYPE_IS ( (kumi::result::cartesian_product_t<kumi::tuple<f1&,f2,f3 const&>>)
+              , (kumi::tuple<kumi::tuple<int&>,kumi::tuple<float>,kumi::tuple<double const&>>)
+              );
+
+  TTS_TYPE_IS ( (kumi::result::cartesian_product_t<kumi::tuple<f1,f2,f3>, kumi::tuple<f1*,f2*>>)
+              , (kumi::tuple< kumi::tuple<int, int*>  , kumi::tuple<float, int*>  , kumi::tuple<double, int*>
+                            , kumi::tuple<int, float*>, kumi::tuple<float, float*>, kumi::tuple<double, float*>
+                            >)
+              );
+};
+
 TTS_CASE("Check cartesian_product() behavior")
 {
   TTS_EQUAL( kumi::cartesian_product(), kumi::tuple<>{} );
@@ -48,6 +70,24 @@ TTS_CASE("Check cartesian_product() behavior with references")
   TTS_EQUAL(kumi::get<0>(kumi::get<2>(cp0)), 10);
 };
 
+TTS_CASE("Check cartesian_product() behavior with references on named tuples")
+{
+  using namespace kumi::literals;
+
+  auto a = "x"_m = 0;
+  using f0 = decltype(a);
+  
+  auto t0 = kumi::tuple<f0,f0&,f0 const&>{a,a,a};
+  auto cp0 = kumi::cartesian_product(t0);
+
+ // kumi::get<0>(kumi::get<0>(cp0))++;
+ // kumi::get<0>(kumi::get<1>(cp0)) = 10;
+
+  TTS_EQUAL(a, 10);
+  TTS_EQUAL(kumi::get<0>(kumi::get<0>(cp0)), 1);
+  TTS_EQUAL(kumi::get<0>(kumi::get<2>(cp0)), 10);
+};
+
 TTS_CASE("Check cartesian_product(ts...) behavior")
 {
   using namespace std::literals;
@@ -57,6 +97,93 @@ TTS_CASE("Check cartesian_product(ts...) behavior")
   auto t3 = kumi::make_tuple("first"s, "second"s, "third"s, "fourth"s);
   auto cp = kumi::cartesian_product(t1,t2,t3);
 
+  TTS_EQUAL( kumi::get<0>(kumi::get< 0>(cp)), kumi::get<0>(t1) );
+  TTS_EQUAL( kumi::get<1>(kumi::get< 0>(cp)), kumi::get<0>(t2) );
+  TTS_EQUAL( kumi::get<2>(kumi::get< 0>(cp)), kumi::get<0>(t3) );
+  TTS_EQUAL( kumi::get<0>(kumi::get< 1>(cp)), kumi::get<1>(t1) );
+  TTS_EQUAL( kumi::get<1>(kumi::get< 1>(cp)), kumi::get<0>(t2) );
+  TTS_EQUAL( kumi::get<2>(kumi::get< 1>(cp)), kumi::get<0>(t3) );
+  TTS_EQUAL( kumi::get<0>(kumi::get< 2>(cp)), kumi::get<0>(t1) );
+  TTS_EQUAL( kumi::get<1>(kumi::get< 2>(cp)), kumi::get<1>(t2) );
+  TTS_EQUAL( kumi::get<2>(kumi::get< 2>(cp)), kumi::get<0>(t3) );
+  TTS_EQUAL( kumi::get<0>(kumi::get< 3>(cp)), kumi::get<1>(t1) );
+  TTS_EQUAL( kumi::get<1>(kumi::get< 3>(cp)), kumi::get<1>(t2) );
+  TTS_EQUAL( kumi::get<2>(kumi::get< 3>(cp)), kumi::get<0>(t3) );
+  TTS_EQUAL( kumi::get<0>(kumi::get< 4>(cp)), kumi::get<0>(t1) );
+  TTS_EQUAL( kumi::get<1>(kumi::get< 4>(cp)), kumi::get<2>(t2) );
+  TTS_EQUAL( kumi::get<2>(kumi::get< 4>(cp)), kumi::get<0>(t3) );
+  TTS_EQUAL( kumi::get<0>(kumi::get< 5>(cp)), kumi::get<1>(t1) );
+  TTS_EQUAL( kumi::get<1>(kumi::get< 5>(cp)), kumi::get<2>(t2) );
+  TTS_EQUAL( kumi::get<2>(kumi::get< 5>(cp)), kumi::get<0>(t3) );
+
+  TTS_EQUAL( kumi::get<0>(kumi::get< 6>(cp)), kumi::get<0>(t1) );
+  TTS_EQUAL( kumi::get<1>(kumi::get< 6>(cp)), kumi::get<0>(t2) );
+  TTS_EQUAL( kumi::get<2>(kumi::get< 6>(cp)), kumi::get<1>(t3) );
+  TTS_EQUAL( kumi::get<0>(kumi::get< 7>(cp)), kumi::get<1>(t1) );
+  TTS_EQUAL( kumi::get<1>(kumi::get< 7>(cp)), kumi::get<0>(t2) );
+  TTS_EQUAL( kumi::get<2>(kumi::get< 7>(cp)), kumi::get<1>(t3) );
+  TTS_EQUAL( kumi::get<0>(kumi::get< 8>(cp)), kumi::get<0>(t1) );
+  TTS_EQUAL( kumi::get<1>(kumi::get< 8>(cp)), kumi::get<1>(t2) );
+  TTS_EQUAL( kumi::get<2>(kumi::get< 8>(cp)), kumi::get<1>(t3) );
+  TTS_EQUAL( kumi::get<0>(kumi::get< 9>(cp)), kumi::get<1>(t1) );
+  TTS_EQUAL( kumi::get<1>(kumi::get< 9>(cp)), kumi::get<1>(t2) );
+  TTS_EQUAL( kumi::get<2>(kumi::get< 9>(cp)), kumi::get<1>(t3) );
+  TTS_EQUAL( kumi::get<0>(kumi::get<10>(cp)), kumi::get<0>(t1) );
+  TTS_EQUAL( kumi::get<1>(kumi::get<10>(cp)), kumi::get<2>(t2) );
+  TTS_EQUAL( kumi::get<2>(kumi::get<10>(cp)), kumi::get<1>(t3) );
+  TTS_EQUAL( kumi::get<0>(kumi::get<11>(cp)), kumi::get<1>(t1) );
+  TTS_EQUAL( kumi::get<1>(kumi::get<11>(cp)), kumi::get<2>(t2) );
+  TTS_EQUAL( kumi::get<2>(kumi::get<11>(cp)), kumi::get<1>(t3) );
+
+  TTS_EQUAL( kumi::get<0>(kumi::get<12>(cp)), kumi::get<0>(t1) );
+  TTS_EQUAL( kumi::get<1>(kumi::get<12>(cp)), kumi::get<0>(t2) );
+  TTS_EQUAL( kumi::get<2>(kumi::get<12>(cp)), kumi::get<2>(t3) );
+  TTS_EQUAL( kumi::get<0>(kumi::get<13>(cp)), kumi::get<1>(t1) );
+  TTS_EQUAL( kumi::get<1>(kumi::get<13>(cp)), kumi::get<0>(t2) );
+  TTS_EQUAL( kumi::get<2>(kumi::get<13>(cp)), kumi::get<2>(t3) );
+  TTS_EQUAL( kumi::get<0>(kumi::get<14>(cp)), kumi::get<0>(t1) );
+  TTS_EQUAL( kumi::get<1>(kumi::get<14>(cp)), kumi::get<1>(t2) );
+  TTS_EQUAL( kumi::get<2>(kumi::get<14>(cp)), kumi::get<2>(t3) );
+  TTS_EQUAL( kumi::get<0>(kumi::get<15>(cp)), kumi::get<1>(t1) );
+  TTS_EQUAL( kumi::get<1>(kumi::get<15>(cp)), kumi::get<1>(t2) );
+  TTS_EQUAL( kumi::get<2>(kumi::get<15>(cp)), kumi::get<2>(t3) );
+  TTS_EQUAL( kumi::get<0>(kumi::get<16>(cp)), kumi::get<0>(t1) );
+  TTS_EQUAL( kumi::get<1>(kumi::get<16>(cp)), kumi::get<2>(t2) );
+  TTS_EQUAL( kumi::get<2>(kumi::get<16>(cp)), kumi::get<2>(t3) );
+  TTS_EQUAL( kumi::get<0>(kumi::get<17>(cp)), kumi::get<1>(t1) );
+  TTS_EQUAL( kumi::get<1>(kumi::get<17>(cp)), kumi::get<2>(t2) );
+  TTS_EQUAL( kumi::get<2>(kumi::get<17>(cp)), kumi::get<2>(t3) );
+
+  TTS_EQUAL( kumi::get<0>(kumi::get<18>(cp)), kumi::get<0>(t1) );
+  TTS_EQUAL( kumi::get<1>(kumi::get<18>(cp)), kumi::get<0>(t2) );
+  TTS_EQUAL( kumi::get<2>(kumi::get<18>(cp)), kumi::get<3>(t3) );
+  TTS_EQUAL( kumi::get<0>(kumi::get<19>(cp)), kumi::get<1>(t1) );
+  TTS_EQUAL( kumi::get<1>(kumi::get<19>(cp)), kumi::get<0>(t2) );
+  TTS_EQUAL( kumi::get<2>(kumi::get<19>(cp)), kumi::get<3>(t3) );
+  TTS_EQUAL( kumi::get<0>(kumi::get<20>(cp)), kumi::get<0>(t1) );
+  TTS_EQUAL( kumi::get<1>(kumi::get<20>(cp)), kumi::get<1>(t2) );
+  TTS_EQUAL( kumi::get<2>(kumi::get<20>(cp)), kumi::get<3>(t3) );
+  TTS_EQUAL( kumi::get<0>(kumi::get<21>(cp)), kumi::get<1>(t1) );
+  TTS_EQUAL( kumi::get<1>(kumi::get<21>(cp)), kumi::get<1>(t2) );
+  TTS_EQUAL( kumi::get<2>(kumi::get<21>(cp)), kumi::get<3>(t3) );
+  TTS_EQUAL( kumi::get<0>(kumi::get<22>(cp)), kumi::get<0>(t1) );
+  TTS_EQUAL( kumi::get<1>(kumi::get<22>(cp)), kumi::get<2>(t2) );
+  TTS_EQUAL( kumi::get<2>(kumi::get<22>(cp)), kumi::get<3>(t3) );
+  TTS_EQUAL( kumi::get<0>(kumi::get<23>(cp)), kumi::get<1>(t1) );
+  TTS_EQUAL( kumi::get<1>(kumi::get<23>(cp)), kumi::get<2>(t2) );
+  TTS_EQUAL( kumi::get<2>(kumi::get<23>(cp)), kumi::get<3>(t3) );
+};
+
+TTS_CASE("Check cartesian_product(ts...) behavior on name dtuples")
+{
+  using namespace std::literals;
+  using namespace kumi::literals;
+
+  auto t1 = kumi::make_tuple("x"_m = 1, "y"_m = 2ULL);
+  auto t2 = kumi::make_tuple("x"_m = 1.2, "y"_m = 3.4f, "z"_m = 5.6);
+  auto t3 = kumi::make_tuple("x"_m = "first"s, "y"_m = "second"s, "z"_m = "third"s, "t"_m = "fourth"s);
+  auto cp = kumi::cartesian_product(t1,t2,t3);
+  
   TTS_EQUAL( kumi::get<0>(kumi::get< 0>(cp)), kumi::get<0>(t1) );
   TTS_EQUAL( kumi::get<1>(kumi::get< 0>(cp)), kumi::get<0>(t2) );
   TTS_EQUAL( kumi::get<2>(kumi::get< 0>(cp)), kumi::get<0>(t3) );
